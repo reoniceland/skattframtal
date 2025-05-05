31a876ade38f19da864716387113b5b4d9481f1a
Erna Guðrún Sigurðardóttir <erna@reon.is>
2025-05-05 23:38:17 +0000

    feat: layout

--- apps/test-app/src/app/layout.tsx
+++ apps/test-app/src/app/layout.tsx
@@ -2,6 +2,8 @@ import { ReactNode } from 'react'
 
 import '@reon-island/ui-core/src/styles/global.css'
 
+import { GridContainer, Header, Page } from '@reon-island/ui-core';
+
 export const metadata = {
   title: 'My App',
 }
@@ -9,7 +11,19 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Page>
+          <GridContainer>
+            <Header
+              info={{
+                title: 'Institution name',
+                description: 'Application name',
+              }}
+            />
+            <main>{children}</main>
+          </GridContainer>
+        </Page>
+      </body>
     </html>
   )
 }
--- apps/test-app/src/app/page.tsx
+++ apps/test-app/src/app/page.tsx
@@ -1,15 +1,11 @@
-'use client'
+'use client';
 
-import { Button } from '@reon-island/ui-core'
+import { Text } from '@reon-island/ui-core';
 
 export default function Home() {
   return (
-    <main>
-      <h1>Welcome to Next.js!</h1>
-      <p>Get started by editing [app]/page.tsx</p>
-      <Button variant="primary">Hello</Button>
-      <Button variant="ghost">Hello</Button>
-      <Button variant="text">Hello</Button>
-    </main>
-  )
+    <>
+      <Text variant="h1">Skil á skattframtali</Text>
+    </>
+  );
 }
