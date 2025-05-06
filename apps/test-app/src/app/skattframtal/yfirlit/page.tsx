e7b7b47291a010eda91f23dde758e1901b81106e
Matthias Skjoldur Sigurdsson <matthias@reon.is>
2025-05-06 20:08:09 +0000

    feat: added tax form overview step

--- apps/test-app/src/app/skattframtal/yfirlit/page.tsx
+++ apps/test-app/src/app/skattframtal/yfirlit/page.tsx
@@ -2,42 +2,49 @@
 
 import Link from 'next/link'
 
-import { Button, CategoryCard, Stack } from '@reon-island/ui-core'
+import type { IconMapIcon } from '@reon-island/ui-core'
+
+import { Button, CategoryCard, Icon, Stack, Text } from '@reon-island/ui-core'
 
 export default function Yfirlit() {
+  const handleIcon = (icon: IconMapIcon) => {
+    return (
+      <Icon icon={icon} size="large" type="outline" color="backgroundBrand" />
+    )
+  }
   return (
     <Stack space={4}>
-      <h1>Hæ Jökull</h1>
-      <p>Hér er einhver texti sem útskýrir yfirlitið...</p>
+      <Text variant="h2">Hæ Jökull</Text>
+      <Text variant="default">
+        Hér er einhver texti sem útskýrir að þetta sé yfirlitið yfir skattamálin
+        þín 2024 sem við ætlum að fara yfir í þessu framtali..
+      </Text>
 
       <CategoryCard
         heading="Jökull"
-        text="120389-4569Bláfjallagata 12, 105 Reykjavíkjokull. thordarson@email. is772-8391"
+        text="120389-4569
+        Bláfjallagata 12, 105 Reykjavík"
       />
       <CategoryCard
-        heading="Jökull"
-        text="120389-4569Bláfjallagata 12, 105 Reykjavíkjokull. thordarson@email. is772-8391"
+        icon={handleIcon('wallet')}
+        heading="Tekjur"
+        text="Heildartekjur þínar árið 2024 voru 10.585.000 kr., þar af 10.260.000 kr. í laun."
       />
       <CategoryCard
-        heading="Jökull"
-        text="120389-4569Bláfjallagata 12, 105 Reykjavíkjokull. thordarson@email. is772-8391"
+        icon={handleIcon('home')}
+        heading="Eignir"
+        text="Heildareignir þínar í lok árs 2024 voru 55.530.000 kr., þar af 1 fasteign og 2 bifreiðar."
       />
       <CategoryCard
-        heading="Jökull"
-        text="120389-4569Bláfjallagata 12, 105 Reykjavíkjokull. thordarson@email. is772-8391"
+        icon={handleIcon('hammer')}
+        heading="Skuldir"
+        text="Heildarskuldir þínar í lok árs 2024 voru 29.801.594 kr."
       />
-      <CategoryCard
-        heading="Jökull"
-        text="120389-4569Bláfjallagata 12, 105 Reykjavíkjokull. thordarson@email. is772-8391"
-      />
-      <CategoryCard
-        heading="Jökull"
-        text="120389-4569Bláfjallagata 12, 105 Reykjavíkjokull. thordarson@email. is772-8391"
-      />
-
-      <Button>
-        <Link href="/tax-form/income">Hefja yfirferð →</Link>
-      </Button>
+      <div>
+        <Button>
+          <Link href="/skattframtal/tekjur">Hefja yfirferð →</Link>
+        </Button>
+      </div>
     </Stack>
   )
 }
