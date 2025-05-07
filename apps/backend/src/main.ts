f17a19e14a0097f979de979fd68847cc0f3fd3d0
Gunnar Cortes Heimisson <gnarr.c@gmail.com>
2025-05-07 15:41:30 +0000

    fix: listen on all interfaces

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .build()
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, document)
-  await app.listen(process.env.port ?? 3000)
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0')
 }
 
 // eslint-disable-next-line
