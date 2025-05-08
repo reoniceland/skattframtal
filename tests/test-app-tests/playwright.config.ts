125cdb5d705137b38637d763616758b1a4efd51b
Matthias Skjoldur Sigurdsson <matthias@reon.is>
2025-05-08 10:50:10 +0000

    test

--- tests/test-app-tests/playwright.config.ts
+++ tests/test-app-tests/playwright.config.ts
@@ -32,10 +32,4 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
   ],
-
-  webServer: {
-    command: 'npm run start',
-    url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI,
-  },
 })
