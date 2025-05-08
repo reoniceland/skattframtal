5f6aa6fa5db672948fa5ae05f2ebf8e8c23a79b4
Matthias Skjoldur Sigurdsson <matthias@reon.is>
2025-05-08 11:06:21 +0000

    test

--- tests/test-app-tests/src/base-tests.spec.ts
+++ tests/test-app-tests/src/base-tests.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '@playwright/test'
 
 test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/')
+  await page.goto(`${process.env.WEB_BASE_URL}`)
 })
